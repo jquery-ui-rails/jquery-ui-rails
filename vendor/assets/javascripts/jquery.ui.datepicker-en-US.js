a82ba8ec9083783756e3c032538af1b2f79ba68a
Ashish Dixit <tundal45@gmail.com>
2013-03-07 07:42:25 -0600

    Create a en-US initialization file for datepicker.

---
+++ vendor/assets/javascripts/jquery.ui.datepicker-en-US.js
@@ -0,0 +1,23 @@
+/* English/US initialization for the jQuery UI date picker plugin. */
+/* Written by Ashish. */
+jQuery(function($){
+	$.datepicker.regional['en-US'] = {
+		closeText: 'Done',
+		prevText: 'Prev',
+		nextText: 'Next',
+		currentText: 'Today',
+		monthNames: ['January','February','March','April','May','June',
+		'July','August','September','October','November','December'],
+		monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+		'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+		dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+		dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+		dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
+		weekHeader: 'Wk',
+		dateFormat: 'mm/dd/yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+	$.datepicker.setDefaults($.datepicker.regional['en-US']);
+});
